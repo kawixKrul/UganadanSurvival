aa6273baea6db6365b3a25e1cbd04863690a7c00
kawixKrul <macioukos@gmail.com>
2023-12-27 11:07:11 +0100

    setup my branch

--- src/main/java/agh/ics/oop/UgandanSurvival.java
+++ src/main/java/agh/ics/oop/UgandanSurvival.java
@@ -3,6 +3,6 @@ package agh.ics.oop;
 public class UgandanSurvival {
     public static void main(String[] args) {
         System.out.println("Start");
-        System.out.println("Stop");]
+        System.out.println("Stop");
     }
 }
