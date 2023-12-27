bdb74627a7b7c432b6e41dcca035bc0edf32fa61
kawixKrul <macioukos@gmail.com>
2023-12-27 11:06:01 +0100

    setup

---
+++ src/main/java/agh/ics/oop/UgandanSurvival.java
@@ -0,0 +1,8 @@
+package agh.ics.oop;
+
+public class UgandanSurvival {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        System.out.println("Stop");]
+    }
+}
